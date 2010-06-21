d5cf04ddc04efb467840a40b4c483f19d9764f63
Joel Meador <joel@expectedbehavior.com>
2010-06-21 14:03:26 -0400

    create, update, destroy callbacks

--- lib/redmine_project_burndown_callbacks/issue_patch.rb
+++ lib/redmine_project_burndown_callbacks/issue_patch.rb
@@ -2,6 +2,10 @@ module RedmineProjectBurndownCallbacks
 
   # patch in some after_save filter action
   module IssuePatch
+    PB_FINISHED  = "finished"
+    PB_STARTED   = "started"
+    PB_UNSTARTED = "unstarted"
+
     def self.included(base)
       base.extend(ClassMethods)
       
@@ -27,20 +31,36 @@ module RedmineProjectBurndownCallbacks
   
 
   module InstanceMethods
-    def push_to_project_burndown
+    def push_to_project_burndown(status = Issue::PB_UNSTARTED)
+      remote_story = ProjectBurndown::Api::V1::RemoteStory.new
+      remote_story.attributes = { "current_state" => self.project_burndown_status, "id" => self.id, "name" => self.subject }
+      remote_story.prefix_options = { :project_id => 1, :service_type_id => "redmine" }
+      remote_story.save
     end
-    
+
     def create_project_burndown_story
-      puts "create"
+      self.push_to_project_burndown
     end
-    
+
     def update_project_burndown_story
-      puts "update"
+      self.push_to_project_burndown(self.project_burndown_status)
     end
-    
+
     def destroy_project_burndown_story
-      puts "destroy"
-    end    
+      remote_story = ProjectBurndown::Api::V1::RemoteStory.new
+      remote_story.attributes = { "id" => self.id }
+      remote_story.prefix_options = { :project_id => 1, :service_type_id => "redmine" }
+      remote_story.destroy
+    end
+
+    # this will have to be configured here for the time being
+    # PB might at some point have a mapping feature
+    def project_burndown_status
+      status = self.reload.status.reload.to_s
+      return self.class::PB_FINISHED if ["Closed"].include?(status)
+      return self.class::PB_STARTED if ["In Progress", "Resolved", "Feedback", "Rejected"].include?(status)
+      self.class::PB_UNSTARTED
+    end
   end
 
 end
