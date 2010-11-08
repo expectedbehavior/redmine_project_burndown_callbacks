79eb80e0918fae8afa32afe479a03858e8d2f094
Joel Meador <joel@expectedbehavior.com>
2010-11-08 13:59:06 -0500

    remove development urls

--- app/models/project_burndown/api/v1/project.rb
+++ app/models/project_burndown/api/v1/project.rb
@@ -1,7 +1,6 @@
 module ProjectBurndown
   class Api::V1::Project < ActiveResource::Base
-    #self.site = "http://#{ProjectBurndown::Config.subdomain}.projectburndown.com"
-    self.site = "http://#{ProjectBurndown::Config.subdomain}.pb.local:10000/api/v1"
+    self.site = "http://#{ProjectBurndown::Config.subdomain}.projectburndown.com/api/v1"
     self.element_name = "project"
     self.timeout = 15
   end
--- app/models/project_burndown/api/v1/story.rb
+++ app/models/project_burndown/api/v1/story.rb
@@ -1,7 +1,6 @@
 module ProjectBurndown
   class Api::V1::Story < ActiveResource::Base
-    #self.site = "http://#{ProjectBurndown::Config.subdomain}.projectburndown.com"
-    self.site = "http://#{ProjectBurndown::Config.subdomain}.pb.local:10000"
+    self.site = "http://#{ProjectBurndown::Config.subdomain}.projectburndown.com"
     self.prefix = "/api/v1"
     self.element_name = "story"
     self.timeout = 15
